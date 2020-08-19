8fce5086931fac9c5d0dd85ae5d5d9c8dcf78c53
rfkinline <robert@lightdec.com>
2020-08-19 14:46:05 -0400

    Excersize 4 and 5 week 3

---
+++ functions isGreaterThan.js
@@ -0,0 +1,9 @@
+function isGreaterThan(num1, num2) {
+  if (num2 > num1) {
+    return true;
+  } 
+  return false;
+}
+
+var output = isGreaterThan(11,10);
+console.log(output); 
---
+++ functions isOldEnoughToDrink.js
@@ -0,0 +1,9 @@
+function isOldEnoughToDrink(age) {
+  if (age >= 21) {
+    return true;
+  } 
+  return false;
+}
+
+var output = isOldEnoughToDrink(22);
+console.log(output); // --> true
