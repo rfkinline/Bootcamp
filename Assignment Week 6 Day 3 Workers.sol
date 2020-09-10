5c35319941bfc9aac586d0a2f6406985d03afb29
rfkinline <robert@lightdec.com>
2020-09-10 18:50:10 -0400

    Week 6 Day 3

--- Assignment Week 6 Day 3 Workers.sol
+++ Assignment Week 6 Day 3 Workers.sol
@@ -9,12 +9,15 @@ contract Workers is People{
     
     function createWorker(string memory name, uint age, uint height, uint workerSalary, address workerAddress) public onlyOwner payable {
         require(msg.value >= 1 ether);   
+// ager requirement
         require(age <= 75, "Worker should not be over 75 years.");
+// worker not allowed to be his boss
         require(msg.sender != workerAddress); 
         createPerson(name, age, height); 
         salary[workerAddress] = workerSalary; 
     }
-    
+
+// fire function    
     function firedWorker(address workerAddress) public onlyOwner{
         deletePerson(workerAddress);
         delete salary[workerAddress];
