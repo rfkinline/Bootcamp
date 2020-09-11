cf2131db515bb863e08ae5cd039c4129f5ef376a
rfkinline <robert@lightdec.com>
2020-09-11 12:08:36 -0400

    Assignment Week 6 Day 5 Overflow

---
+++ Assignment Weeg 6 Day 5 token ERC20.sol
@@ -0,0 +1,59 @@
+pragma solidity 0.5.12;
+import "Safemath.sol";
+
+contract ERC20 {
+    
+// underscore to show that a variable is private    
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 private _totalSupply; 
+    address public owner;
+    using SafeMath for uint256;  
+
+    mapping (address => uint256) private _balances;
+ 
+    constructor(string memory name, string memory symbol, uint256 decimals) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+    }
+    
+    function name() public view returns (string memory){
+        return _name;
+    }
+    
+    function symbol() public view returns (string memory){
+        return _symbol;
+    }
+    
+    function decimals() public view returns (uint256){
+        return _decimals;
+    }
+    
+    function totalSupply() public view returns (uint256){
+        return _totalSupply;
+    }
+    
+    function balanceOf(address account) public view returns (uint256){
+        return _balances[account];
+    }
+
+    function mint(address account, uint256 amount) public {
+    // the 0 addess is non-existant and it is often used to burn tokens
+        require(msg.sender == owner);
+        require(account != address(0), "mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool){
+        require(recipient != address(0), "transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+        return true;
+    }    
+}
---
+++ solodity error overflow underflow.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.7.0;
+import "Safemath.sol";
+
+// uint8 8 bits in size. biggest number is 255. from 0
+
+contract underflow{
+// include "using ...", change all uint8 to uint 256, change calculatios from + to .add
+    using SafeMath for uint256;    // to avid overflow use SafeMath
+    uint256 n = 0;
+    function add() public returns (uint256){
+        n = n.add(1);
+        return n;
+    }
+    function substract() public returns (uint256){
+        n = n.sub(1);
+        return n;
+    }
+
+// Calculation without SafeMath    
+    contract underflow{
+    uint8 n = 0;
+    function add() public returns (uint8){
+        n += 1;
+        return n;
+    }
+    function substract() public returns (uint8){
+        n -= 1;
+        return n;
+    }
+    
+}
