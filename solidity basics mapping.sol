24352c34702cddd51d3ca9050ae82f7046d177e3
rfkinline <robert@lightdec.com>
2020-09-11 09:28:37 -0400

    documentation

--- solidity basics mapping.sol
+++ solidity basics mapping.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.7.0;
-// SPDX-License-Identifier: UNLICENSED
+
+// Mapping is like a better array. A better data structure where we don't have to remember a position in an array or have to look through an entire array to find a person.
+// 
 
 contract HelloWorld{
     struct Person {
@@ -9,11 +11,18 @@ contract HelloWorld{
         bool senior;
     }
     
+    // address = key type and it points to Person. We give mapping an address and it gives me a Person back. Name of mapping is people
     mapping(address => Person) private people;
 
-    function addPerson(string memory name, uint age, uint height, bool senior) public{
+// there are two ways to give a mapping the address:
+
+//  function add Person(address creator, string memory name, uint age, uint height, bool senior) public{   // problem: I as a user can input any address
+        function addPerson(string memory name, uint age, uint height, bool senior) public{
         address creator = msg.sender;
+
+// this is how things are added to the mapping. With an array I needed .push
         people[creator] = Person(name, age, height, senior);
+// the id from the previous array is not needed anymore        
         if(age > 65){
             people[creator].senior=true;
         }
